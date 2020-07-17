60187540ca8ead3ad71b042272d47dbcb9d4ee18
ebertmota <ebert.mota03@gmail.com>
2020-07-16 21:54:08 -0300

    finishing dashboard interface

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/github-logo.svg';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório..." />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/48302018?s=460&v=4"
+            alt="Ebert Mota"
+          />
+          <div>
+            <strong>ebertmota/tracking-corona</strong>
+            <p>
+              React native app to show number of cases ,suspects, and deaths by
+              coronavirus in Brazil.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/48302018?s=460&v=4"
+            alt="Ebert Mota"
+          />
+          <div>
+            <strong>ebertmota/tracking-corona</strong>
+            <p>
+              React native app to show number of cases ,suspects, and deaths by
+              coronavirus in Brazil.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/48302018?s=460&v=4"
+            alt="Ebert Mota"
+          />
+          <div>
+            <strong>ebertmota/tracking-corona</strong>
+            <p>
+              React native app to show number of cases ,suspects, and deaths by
+              coronavirus in Brazil.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
