c1c1e369887a954f0ea93589ada2ee2017e5f081
ebertmota <ebert.mota03@gmail.com>
2020-07-22 21:53:00 -0300

    ending app core functionalities

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,66 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
-import logoImg from '../../assets/github-logo.svg';
+import api from '../../services/api';
 
-import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/github-logo.svg';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      return setInputError('Digite autor/nome do repositório');
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setInputError('');
+      setNewRepo('');
+    } catch (err) {
+      setInputError('Erro ao buscar repositório');
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório..." />
+      <Form hasError={!!inputError} onSubmit={e => handleAddRepository(e)}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório..."
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/48302018?s=460&v=4"
-            alt="Ebert Mota"
-          />
-          <div>
-            <strong>ebertmota/tracking-corona</strong>
-            <p>
-              React native app to show number of cases ,suspects, and deaths by
-              coronavirus in Brazil.
-            </p>
-          </div>
+        {repositories.map(repository => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/48302018?s=460&v=4"
-            alt="Ebert Mota"
-          />
-          <div>
-            <strong>ebertmota/tracking-corona</strong>
-            <p>
-              React native app to show number of cases ,suspects, and deaths by
-              coronavirus in Brazil.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/48302018?s=460&v=4"
-            alt="Ebert Mota"
-          />
-          <div>
-            <strong>ebertmota/tracking-corona</strong>
-            <p>
-              React native app to show number of cases ,suspects, and deaths by
-              coronavirus in Brazil.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
       </Repositories>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -10,7 +14,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -23,6 +27,15 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 1px solid #fff;
+    border-right: 0;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #f54c4c;
+      `}
+
     &::placeholder {
       color: #a8a8b3;
     }
@@ -42,6 +55,12 @@ export const Form = styled.form`
       background: ${shade(0.2, '#04d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #f54c4c;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
@@ -75,7 +94,8 @@ export const Repositories = styled.div`
     }
 
     div {
-      margin-left: 16px;
+      flex: 1;
+      margin: 0 16px;
 
       strong {
         font-size: 20px;
